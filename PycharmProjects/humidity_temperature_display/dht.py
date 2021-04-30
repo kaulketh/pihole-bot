abdaffc12d1e816b56db0de8632edbbf93959df8
Thomas <kaulketh@gmail.com>
2021-04-30 14:59:56 +0200

    Initial commit

---
+++ PycharmProjects/humidity_temperature_display/dht.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# -----------------------------------------------------------
+# dht.py
+# created 30.04.2021
+# Thomas Kaulke, kaulketh@gmail.com
+# https://github.com/kaulketh
+# -----------------------------------------------------------
---
+++ PycharmProjects/humidity_temperature_display/oled.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# -----------------------------------------------------------
+# oled.py
+# created 30.04.2021
+# Thomas Kaulke, kaulketh@gmail.com
+# https://github.com/kaulketh
+# -----------------------------------------------------------
