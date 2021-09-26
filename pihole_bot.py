24a1987bebf2eeef12ab1ef2282e07886e48dcfb
Thomas <44971681+kaulketh@users.noreply.github.com>
2021-09-26 22:55:47 +0200

    Create pihole_bot.py

---
+++ pihole_bot.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------
+# pihole_bot
+# created 26.09.2021
+# Thomas Kaulke, kaulketh@gmail.com
+# https://github.com/kaulketh
+# -----------------------------------------------------------
