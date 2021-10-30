59015799a747cf250abf7c8e67bf4beac1aa9364
Thomas <kaulketh@gmail.com>
2021-10-30 17:25:41 +0200

    implement auto run for restart

---
+++ fritz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------
+# fritz
+# created 30.10.2021
+# Thomas Kaulke, kaulketh@gmail.com
+# https://github.com/kaulketh
+# -----------------------------------------------------------
+
+from resources import reboot
+
+if __name__ == '__main__':
+    reboot.main(3)
